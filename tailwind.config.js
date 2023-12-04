7956c8aa91b45bbbbdac3d516d66cbfd3a1cb02a
Shawn Kost <shawnmkost@gmail.com>
2023-12-04 01:52:11 -0800

    Add ink colors to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
     extend: {
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
-        },
-      },
+        amber: '#F4B300',
+        amethyst: '#80387B',
+        emerald: '#298A34',
+        ruby: '#D2082F',
+        sapphire: '#0089C3',
+        steel: '#9FA9B3'
+      }
     },
   },
   plugins: [],
 }
+export default config
